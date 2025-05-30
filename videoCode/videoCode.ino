4c938b9503132711bf5e9f9d04c4f48cdb8676c8
camila_tite <a00841255@tec.mx>
2025-05-30 15:48:48 -0600

    Añadir archivo de configuración de Arduino y optimizar el código del robot para el seguimiento de mano y evitar obstáculos

--- videoCode/videoCode.ino
+++ videoCode/videoCode.ino
@@ -2,190 +2,159 @@
 #include <NewPing.h>
 #include <Servo.h> 
 
-// Constants
 #define TRIG_PIN A0 
 #define ECHO_PIN A1 
 #define MAX_DISTANCE 200 
 #define MAX_SPEED 190 
-#define MAX_SPEED_OFFSET 20
+#define BUZZER_PIN A4
 
-#define NOTE_C4  262
-#define NOTE_D4  294
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_G4  392
-#define NOTE_A4  440
-#define NOTE_B4  494
-#define NOTE_C5  523
-
-// Buttons
-#define BUTTON_OBSTACLE A2
-#define BUTTON_FOLLOW   A3
-#define BUTTON_SING     A4
-
-// Motors and sensors
 NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+
 AF_DCMotor motor1(1, MOTOR12_1KHZ); 
 AF_DCMotor motor2(2, MOTOR12_1KHZ);
 AF_DCMotor motor3(3, MOTOR34_1KHZ);
 AF_DCMotor motor4(4, MOTOR34_1KHZ);
-Servo myservo;
+
+Servo myservo;   
 
 boolean goesForward = false;
 int distance = 100;
 int speedSet = 0;
-
-int melody[] = {
-  NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4,
-  NOTE_A4, NOTE_A4, NOTE_G4,
-  NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4,
-  NOTE_D4, NOTE_D4, NOTE_C4
-};
-
-int durations[] = {
-  4, 4, 4, 4,
-  4, 4, 2,
-  4, 4, 4, 4,
-  4, 4, 2
-};
-
-enum Mode {
-  NONE,
-  OBSTACLE,
-  FOLLOW,
-  SING
-};
-
-Mode currentMode = NONE;
+int modo = 0;
 
 void setup() {
+  Serial.begin(9600);
   myservo.attach(10);  
-
-  pinMode(BUTTON_OBSTACLE, INPUT_PULLUP);
-  pinMode(BUTTON_FOLLOW, INPUT_PULLUP);
-  pinMode(BUTTON_SING, INPUT_PULLUP);
-
-  myservo.write(115); 
+  myservo.write(115);
+  pinMode(BUZZER_PIN, OUTPUT); 
   delay(2000);
-
   distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
 }
 
 void loop() {
-  checkButtons();
+  int bottom1 = analogRead(A5);
+  int bottom2 = analogRead(A3);
+  int bottom3 = analogRead(A2);
 
-  switch (currentMode) {
-    case OBSTACLE:
-      runObstacleAvoid();
-      break;
-
-    case FOLLOW:
-      followHand();
-      break;
-
-    case SING:
-      playTwinkle();
-      currentMode = NONE;  // Reset to idle after playing
-      break;
-
-    default:
-      moveStop();  // Idle state
-      break;
+  if (modo == 0) {
+    if (bottom1 >= 500) {
+      modo = 1;
+      Serial.println("Modo evitar obstáculos activado");
+      delay(500);
+    } else if (bottom2 >= 500) {
+      modo = 2;
+      Serial.println("Buzzer activado");
+      delay(500);
+    } else if (bottom3 >= 500) {
+      modo = 3;
+      Serial.println("Modo seguir mano activado");
+      delay(500);
+    }
+  } else if (modo == 1) {
+    modeAvoidObstacles();
+    modo = 0;
+  } else if (modo == 2) {
+    playMelody();
+    modo = 0;
+  } else if (modo == 3) {
+    modeFollow();
+    modo = 0;
+  } else {
+    moveStop();
   }
 }
 
-void checkButtons() {
-  if (digitalRead(BUTTON_OBSTACLE) == LOW) {
-    currentMode = OBSTACLE;
-    delay(300); // simple debounce
-  } else if (digitalRead(BUTTON_FOLLOW) == LOW) {
-    currentMode = FOLLOW;
-    delay(300);
-  } else if (digitalRead(BUTTON_SING) == LOW) {
-    currentMode = SING;
-    delay(300);
-  }
-}
+void modeAvoidObstacles() {
+  while (true) {
+    int newBottom1 = analogRead(A5);
+    int newBottom2 = analogRead(A3);
+    int newBottom3 = analogRead(A2);
 
-void runObstacleAvoid() {
-  int distanceR = 0;
-  int distanceL = 0;
-  delay(40);
+    if (newBottom1 >= 500 || newBottom2 >= 500 || newBottom3 >= 500) {
+      moveStop();
+      break;
+    }
 
-  if (distance <= 15) {
-    moveStop();
-    delay(100);
-    moveBackward();
-    delay(300);
-    moveStop();
-    delay(200);
-    distanceR = lookRight();
-    delay(200);
-    distanceL = lookLeft();
-    delay(200);
+    distance = readPing();
+    Serial.print("Distancia: ");
+    Serial.println(distance);
 
-    if (distanceR >= distanceL) {
-      turnRight();
+    if (distance <= 15) {
+      moveStop();
+      delay(100);
+      moveBackward();
+      delay(300);
+      moveStop();
+      delay(200);
+
+      int distanceR = lookRight();
+      delay(200);
+      int distanceL = lookLeft();
+      delay(200);
+
+      if (distanceR >= distanceL) {
+        turnRight();
+      } else {
+        turnLeft();
+      }
+
       moveStop();
     } else {
-      turnLeft();
+      moveForward();
+    }
+
+    delay(100);
+  }
+}
+
+void modeFollow() {
+  while (true) {
+    int newBottom1 = analogRead(A5);
+    int newBottom2 = analogRead(A3);
+    int newBottom3 = analogRead(A2);
+
+    if (newBottom1 >= 500 || newBottom2 >= 500 || newBottom3 >= 500) {
+      moveStop();
+      break;
+    }
+
+    distance = readPing();
+    Serial.print("Distancia a mano: ");
+    Serial.println(distance);
+
+    if (distance > 3 && distance < 30) {
+      moveForward();
+    } else {
       moveStop();
     }
-  } else {
-    moveForward();
-  }
-  distance = readPing();
-}
 
-void followHand() {
-  int d = readPing();
-  if (d >= 10 && d <= 20) {
-    moveForward();
-  } else {
-    moveStop();
+    delay(100);
   }
-}
-
-void playTwinkle() {
-  for (int i = 0; i < sizeof(melody) / sizeof(int); i++) {
-    int noteDuration = 1000 / durations[i];
-    tone(8, melody[i], noteDuration);
-    delay(noteDuration * 1.3);
-    noTone(8);
-  }
-}
-
-// Movement and Sensor Functions
-int readPing() {
-  delay(70);
-  int cm = sonar.ping_cm();
-  if (cm == 0) cm = 250;
-  return cm;
 }
 
 int lookRight() {
   myservo.write(50); 
   delay(500);
-  int distance = readPing();
+  int dist = readPing();
   delay(100);
   myservo.write(115); 
-  return distance;
+  return dist;
 }
 
 int lookLeft() {
   myservo.write(170); 
   delay(500);
-  int distance = readPing();
+  int dist = readPing();
   delay(100);
   myservo.write(115); 
-  return distance;
+  return dist;
+}
+
+int readPing() { 
+  delay(70);
+  int cm = sonar.ping_cm();
+  if (cm == 0) cm = 250;
+  return cm;
 }
 
 void moveStop() {
@@ -193,6 +162,7 @@ void moveStop() {
   motor2.run(RELEASE);
   motor3.run(RELEASE);
   motor4.run(RELEASE);
+  goesForward = false;
 }
 
 void moveForward() {
@@ -241,4 +211,40 @@ void turnLeft() {
   motor3.run(FORWARD);
   motor4.run(FORWARD);   
   delay(500);
+}
+
+void beep(int frequency, int duration) {
+  long delayTime = 1000000L / frequency / 2; 
+  long cycles = (long)frequency * duration / 1000;
+
+  for (long i = 0; i < cycles; i++) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delayMicroseconds(delayTime);
+    digitalWrite(BUZZER_PIN, LOW);
+    delayMicroseconds(delayTime);
+  }
+}
+void playMelody() {
+  int melody[] = {
+    262, 262, 392, 392, 440, 440, 392,
+    349, 349, 330, 330, 294, 294, 262
+  };
+
+  int noteDurations[] = {
+    400, 400, 400, 400, 400, 400, 600,
+    400, 400, 400, 400, 400, 400, 600
+  };
+
+  int notesCount = sizeof(melody) / sizeof(melody[0]);
+
+  for (int i = 0; i < notesCount; i++) {
+    int note = melody[i];
+    int duration = noteDurations[i];
+    if (note == 0) {
+      delay(duration);
+    } else {
+      beep(note, duration);
+    }
+    delay(100);
+  }
 }
